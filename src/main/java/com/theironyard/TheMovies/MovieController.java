32cdf94467db40cf9ee9a688a95f13abc248651c
hamilton moore <hamlmoor@gmail.com>
2017-09-29 13:50:40 -0500

    corrected stream functions

--- src/main/java/com/theironyard/TheMovies/MovieController.java
+++ src/main/java/com/theironyard/TheMovies/MovieController.java
@@ -6,10 +6,15 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.ui.Model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class MovieController {
+    final static String URL = "https://api.themoviedb.org/3/movie";
+    final static String API_KEY = "api_key=be2a38521a7859c95e2d73c48786e4bb";
 
+    final static String nowplayingUrl = URL + "/now_playing" + "?" + API_KEY;
+    final static String allMoviesUrl = URL + "/popular" + "?" + API_KEY;
 
     @RequestMapping("/")
     public String home() {
@@ -18,26 +23,21 @@ public class MovieController {
 
     @RequestMapping("/medium_popular_long_name")
     public String mediumpopularlongname(Model model) {
-        String movieUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb";
+        List<Movie> results = getMovies(allMoviesUrl);
+        results = results.stream()
+                .filter(e -> e.popularity < 800 && e.popularity > 300)
+                .filter(e -> e.title.length() > 10)
+                .collect(Collectors.toList());
 
-        List<Movie> picky = getMovies((movieUrl));
-        picky.stream()
-                .filter(e -> e.popularity < 80 || e.popularity > 30)
-                .filter(e -> e.title.length() > 10);
+        System.out.println(results);  //still doesn;t print
 
-        model.addAttribute("movies", picky);
+        model.addAttribute("pickys", results);
         return "medium_popular_long_name";
     }
 
     @RequestMapping("/now_playing")
     public String nowplaying(Model model) {
-        String movieUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb";
-
-        List<Movie> results = getMovies((movieUrl));
-
-        System.out.println( results == null ? "IT IS NULL!" : "IT's ALIVE!!!" );
-        if (results != null) results.stream().forEach( m -> System.out.println(m.title));
-
+        List<Movie> results = getMovies(nowplayingUrl);
         model.addAttribute("movies", results);
         return "now_playing";
     }
@@ -49,3 +49,6 @@ public class MovieController {
         return movieResult.results;
     }
 }
+
+//System.out.println( results == null ? "IT IS NULL!" : "IT's ALIVE!!!" );
+//        if (results != null) results.stream().forEach( m -> System.out.println(m.title));
